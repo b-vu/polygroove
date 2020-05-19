1bfa980a235119791fed400380ae2d174ce5a8aa
Brian <brianvu7@gmail.com>
2020-05-18 19:48:29 -0500

    changed package jsons and server.js

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const passport = require("passport");
-const routes = require("./routes");
-
-const users = require("./routes/api/users");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// const routes = require("./routes");
+const users = require("./routes/api/users");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3");
 
 // Passport middleware
 app.use(passport.initialize());
@@ -30,6 +24,8 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 // app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3");
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
